9879e58f8f4a42b5d6862f362b09ae1446a5e547
IT-WisamImad <wisam@Wisams-MacBook-Pro.local>
2022-03-12 17:19:03 +0200

    AssigmentOneIUG

---
+++ android/app/src/main/kotlin/com/example/assigment_one_iug/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assigment_one_iug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'assigment_one/Jerusalem.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+      ),
+
+      home: const JerusalemScreen(),
+    );
+  }
+}
